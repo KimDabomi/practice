d030d609b41d8e37117efe2d40d66f6b5ce8b966
Kimdabomi <cdabomi60@gmail.com>
2022-10-26 17:42:05 +0900

    practice

---
+++ react/08-practice/src/pages/CollapseEx.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CollapseEx = () => {
+  return (
+    <div>CollapseEx</div>
+  );
+};
+
+export default CollapseEx;
---
+++ react/08-practice/src/pages/CreateElementEx.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateElementEx = () => {
+  return (
+    <div>CreateElementEx</div>
+  );
+};
+
+export default CreateElementEx;
